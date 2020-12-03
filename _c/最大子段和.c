ca31d696dddbc5c62c092ebcc1dd71141518a9ff
HongShen-Z <com.zhs@gmail.com>
2020-12-03 16:58:41 +0800

    最大子段和

--- _c/最大子段和.c
+++ _c/最大子段和.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int dp_max_sum(int *arr, int n)
+int dp_max_sum(int *arr, int n, int *result)
 {
     int cur_sum = 0, max_sum = 0, i;
 
@@ -9,19 +9,29 @@ int dp_max_sum(int *arr, int n)
         if(cur_sum + arr[i] > 0)
             cur_sum += arr[i];
         else
+        {
             cur_sum = 0;
+            if(i <= result[1])
+                result[0] = i + 1;
+        }
         if(cur_sum > max_sum)
+        {
             max_sum = cur_sum;
+            result[1] = i;
+        }
     }
     return max_sum;
 }
 
 int main()
 {
-    int arr[] = {-2, 11 - 4, 13, -5, -2};
+    int arr[] = {-2, 11, -4, 13, -5, -2};
     int n = sizeof(arr) / sizeof(arr[0]);
+    int result[2] = {0};
 
-    printf("%d", dp_max_sum(arr, n));
+    printf("%d\n", dp_max_sum(arr, n, result));
+    for (int i = result[0]; i <= result[1]; i++)
+        printf("%d ", arr[i]);
 
     return 0;
 }
